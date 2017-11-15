abfb91634f9a33fdaa519b3ab8e2f6805f26340b
Alex S <hlcfan.yan@gmail.com>
2017-11-15 21:21:40 +0800

    Tidy up

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule AliceWiki.Mixfile do
       app: :alice_wiki,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: "Handler for Alice that returns a requested Wikipedia article",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -19,8 +20,6 @@ defmodule AliceWiki.Mixfile do
       mod: {
       Alice, %{
         handlers: [
-          # Alice.Handlers.Random,
-          # Alice.Handlers.AgainstHumanity,
           Alice.Handlers.AliceWiki
         ]
       }
@@ -33,6 +32,17 @@ defmodule AliceWiki.Mixfile do
     [
       {:alice, "~> 0.3"},
       {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      name: :alice_wiki,
+      files: ["lib", "config", "mix.exs", "README*"],
+      maintainers: ["Alex Shi"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hlcfan/alice_wiki",
+              "Docs"   => "https://github.com/hlcfan/alice_wiki"}
     ]
   end
 end
--- test/alice_wiki_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AliceWikiTest do
-  use ExUnit.Case
-  doctest AliceWiki
-
-  test "greets the world" do
-    assert AliceWiki.hello() == :world
-  end
-end
